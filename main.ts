2afc30dba09e2de91296b70acc2fe9317193eb82
GTA Calliope <73078489+elssner@users.noreply.github.com>
2024-09-28 11:29:39 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,29 @@
+pins.onKeyboardEvent(function (zeichenCode, zeichenText, isASCII) {
+    codeeditor.keyboardEvent(zeichenCode, zeichenText)
+})
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    codeeditor.run([
+    "read",
+    "jump lp",
+    "",
+    "lp:",
+    "jump.eq.0 even",
+    "jump.sm.0 odd",
+    "push 2",
+    "sub",
+    "jump lp",
+    "",
+    "even:",
+    "print even!",
+    "halt",
+    "",
+    "odd:",
+    "print odd!",
+    "halt"
+    ])
+})
 matrix.init(matrix.ePages.y128)
 matrix.displayMatrix()
-codeeditor.run([
-"read",
-"jump lp",
-"",
-"lp:",
-"jump.eq.0 even",
-"jump.sm.0 odd",
-"push 2",
-"sub",
-"jump lp",
-"",
-"even:",
-"print even!",
-"halt",
-"",
-"odd:",
-"print odd!",
-"halt"
-])
 loops.everyInterval(50, function () {
     pins.raiseKeyboardEvent(true)
 })
